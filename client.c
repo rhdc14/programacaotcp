52ae7da24ec64f49db77529e8ddad74a36cab3ac
rhdc14 <ra.costa@catolicasc.org.br>
2020-05-29 00:27:05 +0000

    receber dados do servidor

--- client.c
+++ client.c
@@ -1,14 +1,73 @@
 #include <stdio.h>
+#include <string.h> /* strlen */
 #include <sys/socket.h>
+#include <arpa/inet.h> /* inet_addr */
+#include <unistd.h> /* close */
 
 int main(int argc, char *argv[])
 {
-    //* AF_INET - IPv4, SOCK_STREAM - TCP, 0 - IP
-    int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    int socket_desc;
+    struct sockaddr_in server;
+    char *message, server_reply[2000];
 
-    if (socket_desc == -1) {
-    printf("Não foi possivel criar o socket");
-    return 1;
+    /* AF_INET - IPv4, SOCK_STREAM - tcp, 0 - IP */
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0); 
+
+    if (socket_desc == -1)
+    {
+        printf("Não foi possível criar o socket\n");
+        return 1;
     }
+
+    /* 
+    // IPv4 AF_INET sockets:
+    struct sockaddr_in {
+        short            sin_family;   // e.g. AF_INET, AF_INET6
+        unsigned short   sin_port;     // e.g. htons(3490)
+        struct in_addr   sin_addr;     // see struct in_addr, below
+        char             sin_zero[8];  // zero this if you want to
+    };
+    struct in_addr {
+        unsigned long s_addr;          // load with inet_pton()
+    };
+    struct sockaddr {
+        unsigned short    sa_family;    // address family, AF_xxx
+        char              sa_data[14];  // 14 bytes of protocol address
+    };
+    */
+    /* informa os dados do servidor para onde vamos nos conectar */
+    server.sin_addr.s_addr = inet_addr("172.217.28.132"); /* converte IP para long */
+    server.sin_family = AF_INET; /* ip v4 */
+    server.sin_port = htons(80);
+
+    /* conecta-se ao servidor */
+    if (connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0)
+    {
+        printf("Erro ao conectar-se\n");
+        return 1;
+    }
+
+    printf("Conectado.\n");
+
+    /* envia dados */
+    message = "GET / HTTP/1.1\r\n\r\n";
+    if (send(socket_desc, message, strlen(message), 0) < 0)
+    {
+        printf("Erro ao enviar\n");
+        return 1;
+    }
+    printf("Dados enviados.\n");
+
+    /* recebe dados do servidor */
+    if (recv(socket_desc, server_reply, 2000, 0) < 0)
+    {
+        printf("Falha no recv\n");
+        return 1;
+    }
+    printf("Resposta recebida.\n");
+    printf("%s\n", server_reply);
+
+    close(socket_desc);
+
     return 0;
 }
